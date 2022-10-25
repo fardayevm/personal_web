6394f78072be694e3f7ea7624b67bc1495af7826
ali <fardayevm@hartwick.edu>
2022-10-24 21:54:31 -0700

    final project

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
 
-app=Flask(__name__)
+application = Flask(__name__)
 # @app.route('/')
 # def hello_world():
 #     return "Hello World! Ali"
-@app.route('/')
+@application.route('/')
 def my_home():
     return render_template('index.html')
 
-@app.route('/<string:page_name>')
+@application.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
@@ -23,7 +23,7 @@ def write_to_csv(data):
         csv_writer.writerow([email,subject, message])
 
 
-@app.route('/submit_form', methods = ['POST','GET'])
+@application.route('/submit_form', methods = ['POST','GET'])
 def submit_form():
     if request.method=='POST':
         data=request.form.to_dict()
